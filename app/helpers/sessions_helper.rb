5962688d30103602ddb2e157f591efe0a8a8cfa6
quangvungoc <quangvn.f@gmail.com>
2014-01-10 15:19:50 +0700

    add user microposts

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -30,6 +30,13 @@ module SessionsHelper
       user == current_user
     end
 
+    def signed_in_user
+      unless signed_in?
+        store_location
+        redirect_to signin_url, notice: "Please sign in."
+      end
+    end
+
   	def sign_out
   		# change remember_token for security reason as the cookies may be
   		# stolen since normally deleting the cookies from the browser should
