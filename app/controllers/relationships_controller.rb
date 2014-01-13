fe2feb6d46ff52015a3c468048e749845c9650c0
quangvungoc <quangvn.f@gmail.com>
2014-01-13 15:30:14 +0700

    fix bug

--- app/controllers/relationships_controller.rb
+++ app/controllers/relationships_controller.rb
@@ -1,13 +1,12 @@
 class RelationshipsController < ApplicationController
 	before_action :signed_in_user
+	
+  respond_to :html, :js
 
 	def create
 		@user = User.find(params[:relationship][:followed_id])
 		current_user.follow!(@user)
-		respond_to do |format|
-			format.html { redirect_to @user }
-			format.js
-		end
+		respond_with @user
 	end
 
 	# we can destroy the relationship right here
@@ -15,9 +14,6 @@ class RelationshipsController < ApplicationController
 	def destroy
 		@user = Relationship.find(params[:id]).followed
 		current_user.unfollow!(@user)
-		respond_to do |format|
-			format.html { redirect_to @user }
-			format.js
-		end
+		respond_with @user
 	end
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -37,14 +37,14 @@ class UsersController < ApplicationController
   end
 
 	def create
-	    @user = User.new(user_params)    # Not the final implementation!
-	    if @user.save
-          sign_in @user
-          flash[:success] = "Welcome to the Sample App!"
-	      	redirect_to @user
-	    else
-	      	render 'new'
-	    end
+    @user = User.new(user_params)    # Not the final implementation!
+    if @user.save
+      sign_in @user
+      flash[:success] = "Welcome to the Sample App!"
+    	redirect_to @user
+    else
+    	render 'new'
+    end
 	end
 
   def following
